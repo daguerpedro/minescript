d809efa4928e7eb5c1c2b3d06125898166ef26f7
Pedro Daguer <158527683+daguerpedro@users.noreply.github.com>
2024-02-11 13:19:35 -0300

    Update feed.py

--- feed.py
+++ feed.py
@@ -9,7 +9,7 @@ command = '/feed'
 def feed(): 
     minescript.echo('[§2+§r] §eStarting autofeed.')
     lock.acquire()    
-    while(minescript.world_properties()['address'] == 'mccentral.org' or minescript.world_properties()['address'] == 'localhost'):
+    while(minescript.world_properties()['address'] == 'EXAMPLE.SERVER.IP' or minescript.world_properties()['address'] == 'localhost'):
         minescript.execute(command)
         minescript.echo('[§2+§r] §eSuccessfully feed.')
         time.sleep(random.randrange(1 * 60, 5 * 60))
